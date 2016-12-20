401d4226f15697eac3c434b98fe559bb51c96100
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-20 15:31:56 +0100

    uzupelnienie Biletu

--- src/Bilet.java
+++ src/Bilet.java
@@ -2,4 +2,52 @@
  * Created by Torak28 on 20.12.2016.
  */
 public class Bilet {
+    private int typ;
+    private int miastoPoczatkowe;
+    private int miastoKoncowe;
+    private int data;
+    private int godzina;
+
+    public int getTyp(){
+        return typ;
+    }
+    public boolean setTyp(int T){
+        typ = T;
+        return true;
+    }
+    public int getMiastoPoczatkowe(){
+        return miastoPoczatkowe;
+    }
+    public boolean setMiastoPoczatkowe(int MP){
+        miastoPoczatkowe = MP;
+        return true;
+    }
+    public int getMiastoKoncowe(){
+        return miastoKoncowe;
+    }
+    public boolean setMiastoKoncowe(int MK){
+        miastoKoncowe = MK;
+        return true;
+    }
+    public int getData(){
+        return data;
+    }
+    public boolean setData(int D){
+        data = D;
+        return true;
+    }
+    public int getGodzina(){
+        return godzina;
+    }
+    public boolean setGodzina(int G){
+        godzina = G;
+        return true;
+    }
+    public Bilet(int T, int MP, int MK, int D, int G){
+        typ = T;
+        miastoKoncowe = MK;
+        miastoPoczatkowe = MP;
+        data = D;
+        godzina = G;
+    }
 }
