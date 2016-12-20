12f3b3a1431f18a271e1dce0304e4e3e453a6180
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-20 17:48:32 +0100

    Dodanie szukania Biletu

--- src/Aplikacja.java
+++ src/Aplikacja.java
@@ -11,23 +11,44 @@ public class Aplikacja {
     public boolean dodajSamolot(){
         return true;
     }
-    public boolean  usunSamolot(){
+    public boolean usunSamolot(){
         return true;
     }
-    public boolean  dodajLot(){
+    public boolean dodajLot(){
         return true;
     }
-    public boolean  usunLot(){
+    public boolean usunLot(){
         return true;
     }
-    public boolean  szukajSamolotu(int ID){
+    public boolean szukajSamolotu(int ID){
         return true;
     }
-    public boolean  szukajLotu(int NL){
-        return true;
+    public boolean szukajLotu(int NL){
+        int iloscSamolotow = Loty.size();
+        for (int i = 0; i < iloscSamolotow; i++) {
+            if (Loty.get(i).getNumerLotu() == NL) {
+                System.out.println("Jest");
+                return true;
+            }
+        }
+        return false;
     }
     public  Aplikacja(int a){
-
+        //Dodanie 3 Samolotow o klasie odleglosci 1, ilosci miejsc rownej 100 i stanie true
+        Samolot samolotA = new Samolot(1, 100, true);
+        Flota.add(samolotA);
+        Samolot samolotB = new Samolot(1, 100, true);
+        Flota.add(samolotB);
+        Samolot samolotC = new Samolot(1, 100, true);
+        Flota.add(samolotC);
+        //Doadnie Lotow na trasach:
+        //Trasy 1-2, 2-3, 3-1
+        Lot lot12 = new Lot(12, 0, 0, Flota.get(0));
+        Lot lot23 = new Lot(23, 0, 0, Flota.get(1));
+        Lot lot31 = new Lot(31, 0, 0, Flota.get(2));
+        Loty.add(lot12);
+        Loty.add(lot23);
+        Loty.add(lot31);
     }
     //Tymczasowe wywołanie
     public static void main(String[] args) {
@@ -36,6 +57,8 @@ public class Aplikacja {
          * Rezerwacja Biletu
          * Dodanie Lotu
          */
+        Aplikacja aplikacja = new Aplikacja(0);
+        aplikacja.szukajLotu(12);
         System.out.println("Hello, World");
     }
 }
