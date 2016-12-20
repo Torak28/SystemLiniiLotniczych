2edd9f3bf06a5d6a781beb2a70500976b35886a5
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-20 14:59:25 +0100

    Dodanie Aplikacji

--- src/Aplikacja.java
+++ src/Aplikacja.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+
 /**
  * Created by Torak28 on 20.12.2016.
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aplikacja {
+    public List<String>  Flota = new ArrayList<String>();
+    public List<String>  Loty = new ArrayList<String>();
+
+    private boolean dodajSamolot(){
+        return true;
+    }
+    private boolean  usunSamolot(){
+        return true;
+    }
+    private boolean  dodajLot(){
+        return true;
+    }
+    private boolean  usunLot(){
+        return true;
+    }
+    private boolean  szukajSamolotu(int ID){
+        return true;
+    }
+    private boolean  szukajLotu(int NL){
+        return true;
+    }
+    public  Aplikacja(int a){
+
+    }
+    //Tymczasowe wywołanie
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
