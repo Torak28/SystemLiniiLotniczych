939b1a572e562335a62248698e640f0fb5687325
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-01-23 18:45:29 +0100

    Dodanie Test

---
+++ test/DodawanieBiletu.java
@@ -0,0 +1,30 @@
+import java.io.ByteArrayInputStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Torak28 on 23.01.2017.
+ */
+public class DodawanieBiletu {
+	@org.junit.Test
+	public void dodajBilet() throws Exception{
+		Aplikacja app = new Aplikacja();
+		app.dodajSamolot("ErBas",1,400,true);
+		//Ustawienie Scannera na wejście
+		ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
+		System.setIn(in);
+		app.dodajLot(1,2,10,10);
+
+		app.szukajLotu(app.getLoty().get(0).generujNumerLotu()).dodajBiletKupiony(app.szukajLotu(app.getLoty().get(0).generujNumerLotu()).getMiastoPoczatkowe(),app.szukajLotu(app.getLoty().get(0).generujNumerLotu()).getMiastoKoncowe(), app.szukajLotu(app.getLoty().get(0).generujNumerLotu()).getData(), app.szukajLotu(app.getLoty().get(0).generujNumerLotu()).getGodzina() );
+
+		assertEquals(1, app.getLoty().get(0).getIloscKupionych());
+		assertEquals(0, app.getLoty().get(0).getIloscZarezerwowanych());
+		assertEquals(app.getLoty().get(0).getBilety().get(0).getMiastoPoczatkowe(), app.getLoty().get(0).getMiastoPoczatkowe());
+		assertEquals(app.getLoty().get(0).getBilety().get(0).getMiastoKoncowe(), app.getLoty().get(0).getMiastoKoncowe());
+		assertEquals(app.getLoty().get(0).getBilety().get(0).getData(), app.getLoty().get(0).getData());
+		assertEquals(app.getLoty().get(0).getBilety().get(0).getGodzina(), app.getLoty().get(0).getGodzina());
+		assertEquals(app.getLoty().get(0).getNumerLotu(), app.getLoty().get(0).getNumerLotu());
+		assertEquals(app.getLoty().get(0).getSamolot(), app.getLoty().get(0).getSamolot());
+		assertEquals(1,app.getLoty().get(0).getBilety().get(0).getMiejsce());
+	}
+}
